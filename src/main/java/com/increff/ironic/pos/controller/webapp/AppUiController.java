09b205ea93b171dccb0932b83f278e2bb24db1d6
Rounik <rounik1020@gmail.com>
2022-12-22 15:57:33 +0530

    Removed employee feature

--- src/main/java/com/increff/ironic/pos/controller/webapp/AppUiController.java
+++ src/main/java/com/increff/ironic/pos/controller/webapp/AppUiController.java
@@ -1,13 +1,8 @@
 package com.increff.ironic.pos.controller.webapp;
 
-import com.increff.ironic.pos.pojo.Employee;
-import com.increff.ironic.pos.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class AppUiController extends AbstractUiController {
@@ -15,11 +10,6 @@ public class AppUiController extends AbstractUiController {
     @RequestMapping(value = "/ui/home")
     public ModelAndView home() {
         return mav("home.html");
-    }
-
-    @RequestMapping(value = "/ui/employee")
-    public ModelAndView employee() {
-        return mav("employee.html");
     }
 
     @RequestMapping(value = "/ui/brand")
--- src/main/java/com/increff/ironic/pos/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package com.increff.ironic.pos.dao;
-
-import com.increff.ironic.pos.dao.parent.CrudDao;
-import com.increff.ironic.pos.pojo.Employee;
-
-public interface EmployeeDao extends CrudDao<Employee, Integer> {
-}
