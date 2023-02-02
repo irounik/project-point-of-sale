2aa18a57baf23dbf45c58358df4796cbbf2fad19
Rounik <rounik1020@gmail.com>
2023-02-02 11:47:54 +0530

    handling MethodArgumentTypeMismatchException exceptions

--- pos/src/main/java/com/increff/ironic/pos/controller/advice/AppRestControllerAdvice.java
+++ pos/src/main/java/com/increff/ironic/pos/controller/advice/AppRestControllerAdvice.java
@@ -1,39 +1,47 @@
 package com.increff.ironic.pos.controller.advice;
 
+import com.increff.ironic.pos.exceptions.ApiException;
+import com.increff.ironic.pos.model.data.MessageData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.increff.ironic.pos.model.data.MessageData;
-import com.increff.ironic.pos.exceptions.ApiException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class AppRestControllerAdvice {
 
-	@ExceptionHandler(ApiException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public MessageData handle(ApiException e) {
-		MessageData data = new MessageData();
-		data.setMessage(e.getMessage());
-		return data;
-	}
+    @ExceptionHandler(ApiException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageData handle(ApiException e) {
+        MessageData data = new MessageData();
+        data.setMessage(e.getMessage());
+        return data;
+    }
 
-	@ExceptionHandler(Throwable.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public MessageData handle(Throwable e) {
-		MessageData data = new MessageData();
-		data.setMessage("An unknown error has occurred - " + e.getMessage());
-		return data;
-	}
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public MessageData handle(Throwable e) {
+        MessageData data = new MessageData();
+        data.setMessage("An unknown error has occurred - " + e.getMessage());
+        return data;
+    }
 
-	@ExceptionHandler(HttpMessageNotReadableException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public MessageData hande(HttpMessageNotReadableException ex) {
-		MessageData data = new MessageData();
-		data.setMessage("Invalid Input!");
-		return data;
-	}
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageData handleMismatch(MethodArgumentTypeMismatchException e) {
+        MessageData data = new MessageData();
+        data.setMessage("Invalid Input, please check argument type!");
+        return data;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageData hande(HttpMessageNotReadableException ex) {
+        MessageData data = new MessageData();
+        data.setMessage("Invalid Input!");
+        return data;
+    }
 
 }
